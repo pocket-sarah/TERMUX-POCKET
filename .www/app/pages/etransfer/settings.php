11fc4e10427e724edfb89ac4e4a9111960095da9
pocket-sarah <projectsarah25@gmail.com>
2025-10-20 05:12:05 -0600

    Create settings.php

---
+++ .www/app/pages/etransfer/settings.php
@@ -0,0 +1,9 @@
+inteeac settings page
+
+edit config/config.php in documrnt root
+
+sendername 
+sender_name values
+
+‐--------
+scan doc root templates folder to display a drop down of html templstes and when selecting a new template update the email_TEMPLATE value in config file in document root
