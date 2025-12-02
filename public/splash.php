fa26d53d6b65107795d088c9b985ff00a48d0cbc
pocket-sarah <projectsarah25@gmail.com>
2025-12-01 22:57:07 -0700

    Create splash.php

---
+++ public/splash.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Splash</title>
+<style>
+body{background:black;color:#00ffcc;font-family:monospace;text-align:center;padding-top:20vh}
+</style>
+</head>
+<body>
+
+<h1>POCKET SYSTEM</h1>
+<div>Connected to Render</div>
+<div><?= date("Y-m-d H:i:s") ?></div>
+
+</body>
+</html>
